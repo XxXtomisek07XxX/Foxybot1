58a3405957e9f576c713f43192e8e68ac0f34e49
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2018-11-29 18:49:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
 
+import asyncio
+import platform
+import colorsys
+import random
+import os 
+import time
+
+
+
+
+
+bot=commands.Bot(command_prefix='/')
+
+
+
+@bot.event
+async def on_ready():
+     await bot.change_presence(game=discord.Game(name= "/help"))
+     
+   
+        
+
+@bot.event
+async def on_ready():
+    
+
+  print('The bot is ready!')
+  print(bot.user.name)
+  print(bot.user.id)
