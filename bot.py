3501b9f5d354351fd266f2ec8b8de16bcbb02d09
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2018-11-30 19:16:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,66 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType
-
 import asyncio
-import platform
-import colorsys
-import random
-import os 
-import time
 
-
-
-
-
-bot=commands.Bot(command_prefix='/')
-
-
+bot=commands.Bot(command_prefix='f!')
 
 @bot.event
 async def on_ready():
-     await bot.change_presence(game=discord.Game(name= "/help"))
+	print('The bot is ready!')
+	print(bot.user.name)
+	print(bot.user.id)
+	
+@bot.command()
+async def info():
+	await bot.say('Tohoto bota udělal Majitel MegaCraftu/Syn#1308')
+	
+@bot.command()
+async def hosting():
+	await bot.say('****Tento Command Se Dodělává!****')
+	
+@bot.command()
+async def request():
+	await bot.say('****__Děkujeme za Request!__****')
+	
+bot.run('NTE3NzUyNTU3NTQ0OTMxMzMw.DuGyiw.7jpxF_VH3-32vKknQ4Nkzy6DbF4')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
      
    
         
 
-@bot.event
-async def on_ready():
-  print('The bot is ready!')
-  print(bot.user.name)
-  print(bot.user.id)
 
-  print('The bot is ready!')
+
+  
+ 
+ 
+
+
   
   
      
-@bot.command()
-async def support():
-     await bot.say('Dodělává se!')
+
+
+    
      
-bot.run(os.getenv("BOT_TOKEN"))
+
